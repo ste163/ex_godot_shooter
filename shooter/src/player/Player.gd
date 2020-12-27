aa22a40c427efede9e39a25e6c68a3f94c4d7148
ste163 <samtedwards@pm.me>
2020-12-27 16:56:50 -0500

    re-organized project

--- shooter/src/player/Player.gd
+++ shooter/src/player/Player.gd
@@ -1,4 +1,4 @@
-extends KinematicBody
+extends Character
 
 export var mouse_sensitivity: float = 0.5
 
@@ -6,10 +6,16 @@ export var mouse_sensitivity: float = 0.5
 # wait for it to load before continuing.
 # $ is shorthand for that node.
 onready var _camera = $Camera
+var _move_vect: Vector3 = Vector3()
+
+# what to load before starting game
+func _ready() -> void:
+	# hide mouse cursor and lock it to center of screen
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
 
 # _process runs every frame, delta is time since last frame.
 func _process(delta: float) -> void:
-	exitGame();
+	_movementInputs()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -23,7 +29,13 @@ func _mouseInput(event: InputEvent) -> void:
 	_camera.rotation_degrees.x -= mouse_sensitivity * event.relative.y
 	# ensure you can't look passed -90 or 90 degrees
 	_camera.rotation_degrees.x = clamp(_camera.rotation_degrees.x, -90, 90)
-
-func exitGame() -> void:
-	if Input.is_action_just_pressed("exit"):
-		get_tree().quit()
+	
+func _movementInputs() -> void:
+	if Input.is_action_pressed("move_forward"):
+		_move_vect += Vector3.FORWARD
+	if Input.is_action_pressed("move_backward"):
+		_move_vect += Vector3.BACK
+	if Input.action_press("move_left"):
+		_move_vect += Vector3.LEFT
+	if Input.action_press("move_right"):
+		_move_vect += Vector3.RIGHT
