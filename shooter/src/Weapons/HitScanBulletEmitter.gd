3c62ef065b7dc2013166b986c1998a6f30ad179f
ste163 <samtedwards@pm.me>
2020-12-29 10:16:59 -0500

    refactored particle emitter code

--- shooter/src/Weapons/HitScanBulletEmitter.gd
+++ shooter/src/Weapons/HitScanBulletEmitter.gd
@@ -28,21 +28,23 @@ func fire() -> void:
 	if result and result.collider.has_method("hurt"):
 		result.collider.hurt(damage, result.normal) #the normal will spray blood in the hit direction
 	elif result: #if hit something that can't be hurt, instantiate the hit effect
-		var hit_effect_instance = hit_effect.instance()
-		get_tree().get_root().add_child(hit_effect_instance) #get the root of the scene we're in
-		hit_effect_instance.global_transform.origin = result.position #set the hit effect location to the location of where our bullet hit, but has no orientation
+		_createAndOrientParticleEffect(result)
+
+func _createAndOrientParticleEffect(result: Dictionary) -> void:
+	var hit_effect_instance = hit_effect.instance()
+	get_tree().get_root().add_child(hit_effect_instance) #get the root of the scene we're in
+	hit_effect_instance.global_transform.origin = result.position #set the hit effect location to the location of where our bullet hit, but has no orientation
 		
-		#because the particle effect is already oriented up, if we hit a flat surface, don't re-orient the effect
-		if result.normal.angle_to(Vector3.UP) < 0.00005:
-			return
-		if result.normal.angle_to(Vector3.DOWN) < 0.00005:
-			hit_effect_instance.rotate(Vector3.RIGHT, PI) #PI = 180 degrees, so if we hit the ceiling, flip the effect upside down
-			return
+	#because the particle effect is already oriented up, if we hit a flat surface, don't re-orient the effect
+	if result.normal.angle_to(Vector3.UP) < 0.00005:
+		return
+	if result.normal.angle_to(Vector3.DOWN) < 0.00005:
+		hit_effect_instance.rotate(Vector3.RIGHT, PI) #PI = 180 degrees, so if we hit the ceiling, flip the effect upside down			return
 		
 		#cross products - when you have 2 vectors, this will get you what's perpendicular to them
 		#allows us to get a correctly oriented particle effect
-		var y: Vector3 = result.normal
-		var x: Vector3 = y.cross(Vector3.UP)
-		var z: Vector3 = x.cross(y)
+	var y: Vector3 = result.normal
+	var x: Vector3 = y.cross(Vector3.UP)
+	var z: Vector3 = x.cross(y)
 		
-		hit_effect_instance.global_transform.basis = Basis(x, y, z)
+	hit_effect_instance.global_transform.basis = Basis(x, y, z)
