35644e686c1cd20a3d2bf7af0a712b26ecfefdac
ste163 <samtedwards@pm.me>
2020-12-29 17:32:48 -0500

    added bullet spread randomizer for machine gun

--- shooter/src/Weapons/BulletRotater.gd
+++ shooter/src/Weapons/BulletRotater.gd
@@ -7,6 +7,9 @@ extends Emitter_Bullet
 
 onready var _childEmitters: Array = get_children()
 
+func _on_MachineGun_fired() -> void:
+	_spreadRandomly()
+
 func _on_Shotgun_fired() -> void:
 	_rotateRandomly()
 
@@ -22,7 +25,13 @@ func set_bodies_to_exclude(_bodies_to_exclude: Array) -> void:
 func fire() -> void:
 	for e in _childEmitters:
 		e.fire()
-	
+
+func _spreadRandomly() -> void:
+	var neg: float = -0.04
+	var pos: float = 0.04
+	rotation.x = rand_range(neg, pos)
+	rotation.y = rand_range(neg, pos)
+
 func _rotateRandomly() -> void:
 	#must have new rand_ranges every time or else it reuses the same range
 	#and doesn't randomize properly
@@ -31,3 +40,4 @@ func _rotateRandomly() -> void:
 		e.rotation.z = rand_range(0.0, (2 * PI))
 		e.translation.y = rand_range(-0.5, 0.5)
 		e.translation.x = rand_range(-0.7, 0.2)
+
